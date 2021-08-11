4f6b4ec1b3779b0dbdab18a42caa5634a5c60f9e
FicusFocus <shkurihin.pashok@mail.ru>
2021-08-12 05:11:39 +0600

    .

--- UnityHomeWork_5_2/Assets/Scripts/EnemyCreator.cs
+++ UnityHomeWork_5_2/Assets/Scripts/EnemyCreator.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 
 public class EnemyCreator : MonoBehaviour
 {
-    [SerializeField] private List<Transform> _spawnPoints = new List<Transform>();
-    [SerializeField] private Enemy _EnemyTemplate;
+    [SerializeField] private Transform _spawn;
+    [SerializeField] private Enemy _template;
     [SerializeField] private float _waitingTime;
     [SerializeField] private int _maxEnemyCount;
 
+    private Transform[] _spawnPoints;
     private List<Enemy> _enemyes = new List<Enemy>();
-    private float _nextWaitingtime;
+    private float _nextWaitingTime;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[_spawn.childCount];
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+            _spawnPoints[i] = _spawn.GetChild(i);
+    }
 
     private void Update()
     {
-        int randomSpawnPositionNumber = Random.Range(0, _spawnPoints.Count);
-        if (Time.time > _nextWaitingtime)
+        int randomSpawnPositionNumber = Random.Range(0, _spawnPoints.Length);
+        if (Time.time > _nextWaitingTime)
         {
-            _nextWaitingtime += _waitingTime;
+            _nextWaitingTime += _waitingTime;
 
-            var newEnemy = Instantiate(_EnemyTemplate, _spawnPoints[randomSpawnPositionNumber].position, Quaternion.identity);
+            var newEnemy = Instantiate(_template, _spawnPoints[randomSpawnPositionNumber].position, Quaternion.identity);
             _enemyes.Add(newEnemy);
         }        
 
         if (_enemyes.Count > _maxEnemyCount)
         {
             foreach (var enemy in _enemyes)
-            {
                 enemy.AutoDestroy();
-            }
 
             _enemyes.Clear();
         }
