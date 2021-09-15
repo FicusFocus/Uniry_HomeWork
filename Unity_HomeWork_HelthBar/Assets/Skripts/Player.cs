198e788e8c7bd51a61a41b4916e7a14ac1fb627a
FicusFocus <shkurihin.pashok@mail.ru>
2021-09-16 00:49:41 +0600

    .

--- Unity_HomeWork_HelthBar/Assets/Skripts/Player.cs
+++ Unity_HomeWork_HelthBar/Assets/Skripts/Player.cs
@@ -1,33 +1,59 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private Slider _helthBar;
-    [SerializeField]private int _helth;
+    [SerializeField] private int _helth;
+    [SerializeField] private float _duration;
 
-    private int _currentHelth;
+    private float _currentHelth;
+    private float _targetHalth;
+
+    public event UnityAction Hited;
+    public event UnityAction Died;
 
     private void Start()
     {
         _currentHelth = _helth;
+        _targetHalth = _helth;
     }
 
-    public void TakeDamage(int value)
+    private void Update()
     {
-        if (_currentHelth > 0 && value > 0)
+        _helthBar.value = Mathf.MoveTowards(_helthBar.value, _targetHalth, _duration * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (_currentHelth > 0 && damage > 0)
         {
-            _currentHelth -= value;
-            _helthBar.value = _currentHelth;
+            if (_currentHelth - damage < 0)
+            {
+                _targetHalth = 0;
+                _currentHelth = 0;
+            }
+            else
+            {
+                _currentHelth -= damage;
+                _targetHalth -= damage;
+            }
+            
+            Hited?.Invoke();
+        }
+        else if (_currentHelth <= 0)
+        {
+            Died?.Invoke();
         }
     }
 
-    public void HealPlayer(int value)
+    public void HealPlayer(int heal)
     {
-        if (_currentHelth + value <= _helth && value > 0)
+        if (_currentHelth + heal <= _helth && heal > 0)
         {
-            _currentHelth += value;
-            _helthBar.value = _currentHelth;
+            _currentHelth += heal;
+            _targetHalth += heal;
         }
     }
 }
---
+++ Unity_HomeWork_HelthBar/Assets/Skripts/playerAnimationSetter.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class playerAnimationSetter : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Player _player;
+
+    private string _hit = "Hit";
+    private string _die = "Die";
+
+    private void OnEnable()
+    {
+        _player.Died += SetTriggerDie;
+        _player.Hited += SetTriggerHit;
+    }
+
+
+    private void OnDisable()
+    {
+        _player.Died -= SetTriggerDie;
+        _player.Hited -= SetTriggerHit;
+    }
+
+    private void SetTriggerDie()
+    {
+        _animator.SetTrigger(_die);
+    }
+
+    private void SetTriggerHit()
+    {
+        _animator.SetTrigger(_hit);
+    }
+}
