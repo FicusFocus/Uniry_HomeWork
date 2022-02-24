402011149cca421fd6afb9d6b9e0daefe64a040c
FicusFocus <shkurihin.pashok@mail.ru>
2022-02-24 07:08:56 +0600

    .

--- TowerDifence/Assets/Scripts/LevelList/Level2.cs
+++
@@ -1,9 +0,0 @@
-using IJunior.TypedScenes;
-
-public class Level2 : BaceLevel
-{
-    protected override void OnChoiseLevelClick()
-    {
-        LVL2.Load();
-    }
-}
--- TowerDifence/Assets/Scripts/UI/MoneyView.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class MoneyView : MonoBehaviour
-{
-    [SerializeField] private Player _player;
-}
